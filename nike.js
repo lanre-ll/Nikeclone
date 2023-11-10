9a3b64bf0e9d136574da3468e4b3d0f2e7524f29
Lawal Warith <79901863+lanre-ll@users.noreply.github.com>
2023-11-10 10:23:10 +0100

    Add files via upload

--- nike.js
+++ nike.js
@@ -1,11 +1,20 @@
-const contactForm = document.getElementById('contact--form')
+const contactForm = document.getElementById('contact--form'),
+	contactMessage = document.getElementById('contact-message')
 
 const sendEmail = (e) => {
 	e.preventDefault()
 
 	emailjs.sendForm('service_6xoes49','template_s0cw67l','#contact--form','3R4zm0czgp5SqXFAf')
 	.then (() => {
-		contactForm.reset()
+
+		document.querySelector('.contact__message').style.display = 'block';
+
+		contactMessage.textContent = "Thank you! We'll be in touch soon."
+
+		setTimeout(() => {
+			contactMessage.textContent = ''
+		}, 10000)
+		contactForm.remove()
 	})
 }
 
